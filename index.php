a0a2768ce8cc8e58f3a4a4cc592bea2696d98606
Ivo Schwizer <ivoschwizer@gmail.com>
2023-03-30 12:58:37 +0200

    k

--- index.php
+++ index.php
@@ -206,6 +206,10 @@
     </section>
 
     <?php require_once 'footer.php'; ?>
-    <?php require_once 'script.php'; ?>
+    <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="js/vendor/jquery-3.6.3.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
 </body>
 </html>
--- script.php
+++ script.php
@@ -1,4 +1,3 @@
 <script src="js/vendor/modernizr-3.6.0.min.js"></script>
 <script src="js/vendor/jquery-3.6.3.min.js"></script>
 <script src="js/plugins.js"></script>
-<script src="js/main.js"></script>
