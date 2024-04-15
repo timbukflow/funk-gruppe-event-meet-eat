410dcc972bec102c5d3ba60e2fd9cb97f083d93e
Ivo Schwizer <ivoschwizer@gmail.com>
2024-04-15 17:11:55 +0200

    2024 fix

--- form.php
+++ form.php
@@ -128,7 +128,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
          
         $headers = "From: anmeldung@funk-gruppe-event.ch";
-        $to = "ivoschwizer@gmail.com";
+        $to = "bern@funk-gruppe.ch";
         $subject = "Funk Gruppe Event | meet&eat";
         $headers .= "\r\nContent-Type: text/plain; charset=utf-8\r\n";
             
