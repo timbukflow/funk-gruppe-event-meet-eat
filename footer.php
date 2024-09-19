47b171e4dfdd5a82643479e2cc37f0c9d96cf089
Ivo Schwizer <ivoschwizer@gmail.com>
2024-09-19 07:20:02 +0200

    Form and Footer

--- footer.php
+++ footer.php
@@ -1,18 +1,17 @@
 <footer>
-    <div class="footer02">
-        <div>
-            <p>
-                Funk Bern <br>
-                Feldstrasse 42 <br>
-                CH-3073 Gümligen <br>
-            </p>
-        </div>
-        <div> 
+    <div class="footer01">
+        <p>
+            Funk Bern <br>
+            Feldstrasse 42 <br>
+            CH-3073 Gümligen <br> <br>
             <a href="tel:041583110200">T +41 58 311 02 00</a>
             <a href="https://www.funk-gruppe.ch/de/" target="_top">www.funk-gruppe.ch</a>
-        </div>
+        </p>
     </div>
-    <div class="footer01">
+    <div class="footer02">
         <h1>Die beste Empfehlung. Funk.</h1>
+        <div class="footer-logo">
+            <img src="img/funk-logo-w.svg" alt="funk logo">
+        </div> 
     </div> 
 </footer>
--- index.php
+++ index.php
@@ -14,31 +14,36 @@
     <header>
         <!-- <div class="asheader"><h3>Anmeldeschluss ist der 12. April 2023.</h3> </div> -->
         <div class="event-title"><img src="./img/funk-meet-eat.svg" alt="funk-logo"></div>
-        <button class="homebutton"> <a class="goto" href="#anmeldung">zur Anmeldung</a> </button>
+        <!-- <button class="homebutton"> <a class="goto" href="#anmeldung">zur Anmeldung</a> </button> -->
     </header>
     <section>
       <article class="intro">
-        <h1>Herzliche Einladung</h1>
+        <h1>Ihr Platz ist reserviert – bald ist es soweit…</h1>
         <p>
           <span>
             Liebe Geschäfts- und Netzwerkpartner
           </span>
           <span>
-            Im Namen der Niederlassung Bern, lade ich Sie herzlich zum Funk meet&#38;eat vom Donnerstag, <strong>17. Oktober 2024 ab 17:00 Uhr im Berner Kursaal ein.</strong>
+            Wir bedanken uns herzlich für Ihre Anmeldung und freuen uns, Sie am <strong>Donnerstag, 17. Oktober 2024 im Berner Kursaal</strong> begrüssen zu dürfen.
           </span>
-        </p>
-        <p class="quot">
-          <span class="title">
-            Mit Keynote von Bernhard Heusler.
+          <span>
+            Freuen Sie sich auf die Keynote von Bernhard Heusler.
+            Bekannt als ehemaliger Präsident des FC Basel, Leadership-Referent und Buchautor.
           </span>
-            Bekannt als ehemaliger Präsident des FC Basel, Leadership-Referent und Buchautor. Ungeachtet ob in Verwaltungsräten, Spitzensport-, Verkaufs- oder Produktions-Teams, sind es schlussendlich immer die Menschen bzw. ihre Aufgaben, Rollen und ihr Zusammenspiel, welches für das Erreichen der gemeinsamen Ziele entscheidend ist. Teamwork und Führung sind also zentrale Erfolgsfaktoren jeder Unternehmung. Unter dem Titel "EIN TEAM GEWINNT IMMER", ist das neue Buch von Bernhard Heusler im Oktober 2023 erschienen. Der Autor persönlich, wird uns sein Leadership-ABC aus erster Hand und auf humorvolle Art und Weise näherbringen. 
+          <span>
+            Ungeachtet ob in Verwaltungsräten, Spitzensport-, Verkaufs- oder Produktions-Teams, sind es schlussendlich immer die Menschen bzw. ihre Aufgaben, Rollen und ihr Zusammenspiel, welche für das Erreichen der gemeinsamen Ziele entscheidend sind.
           </span>
-        </p>
-        <p>
-          Lassen Sie sich inspirieren und geniessen Sie mit uns zusammen einen spannenden Abend mit persönlichen Gesprächen, Networking und kulinarischen Leckerbissen.<br><br>
-          Herzliche Grüsse <br>
-          <strong>Jonas Müller</strong> <br>
-          Leiter Niederlassung Bern
+          <span>
+            Teamwork und Führung sind also zentrale Erfolgsfaktoren jeder Unternehmung. Unter dem Titel &#171;EIN TEAM GEWINNT IMMER&#187;, ist das neue Buch von Bernhard Heusler im Oktober 2023 erschienen. Der Autor persönlich, wird uns sein Leadership-ABC aus erster Hand und auf humorvolle Art und Weise näherbringen.
+          </span>
+          <span>
+            Lassen Sie sich inspirieren und geniessen Sie mit uns zusammen einen spannenden Abend mit persönlichen Gesprächen, Networking und kulinarischen Leckerbissen.
+          </span>
+          <span>
+            Herzliche Grüsse <br>
+            <strong>Jonas Müller</strong> <br>
+            Leiter Niederlassung Bern
+          </span>
         </p>
       </article>
 
@@ -102,113 +107,9 @@
           </div>
         </div>
       </article>
-      <div id="anmeldung" class="containerform">
-            <h2>Anmeldung</h2>
-            <p>Die Teilnehmerzahl ist beschränkt. Die Anmeldungen werden nach Eingang berücksichtigt. <strong>Anmeldeschluss ist der 20. September 2024.</strong></p>
-            <?php include('form.php'); ?>
-
-            <form id="contact" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" novalidate>
-              
-              <fieldset class="checkbox">
-                <div>
-                  <input class="radio" type="radio" id="checkbox1" name="teilnahme" value="Ja, ich nehme gerne teil" tabindex="1" 
-                  <?= (isset($teilnahme) && $teilnahme == "Ja, ich nehme gerne teil") ? "checked" : "" ?>>
-                  <label for="checkbox1">Ja, ich nehme gerne teil</label>
-                </div>
-                <div>
-                  <input class="radio" type="radio" id="checkbox2" name="teilnahme" value="Leider bin ich verhindert" tabindex="2"
-                  <?= (isset($teilnahme) && $teilnahme == "Leider bin ich verhindert") ? "checked" : "" ?>>
-                  <label for="checkbox2">Leider bin ich verhindert</label>
-                </div>
-                <span class="error"><?= isset($errors["teilnahme"]) ? $errors["teilnahme"] : $teilnahme_error ?></span>
-              </fieldset>
-
-              <fieldset>
-                <input placeholder="Vorname&#42;" type="text" name="vorname" value="<?= htmlspecialchars($vorname) ?>" tabindex="5" autofocus>
-                <span class="error"><?= isset($errors["vorname"]) ? htmlspecialchars($errors["vorname"]) : htmlspecialchars($vorname_error) ?></span>
-              </fieldset>
-
-              <fieldset>
-                <input placeholder="Name&#42;" type="text" name="name" value="<?= htmlspecialchars($name) ?>" tabindex="6">
-                <span class="error"><?= isset($errors["name"]) ? htmlspecialchars($errors["name"]) : htmlspecialchars($name_error) ?></span>
-              </fieldset>
-
-              <fieldset>
-                <input placeholder="Firma&#42;" type="text" name="firma" value="<?= htmlspecialchars($firma) ?>" tabindex="7">
-                <span class="error"><?= isset($errors["firma"]) ? htmlspecialchars($errors["firma"]) : htmlspecialchars($firma_error) ?></span>
-              </fieldset>
-
-              <fieldset>
-                <input placeholder="Email&#42;" type="text" name="email" value="<?= htmlspecialchars($email) ?>" tabindex="8">
-                <span class="error"><?= isset($errors["email"]) ? htmlspecialchars($errors["email"]) : htmlspecialchars($email_error) ?></span>
-              </fieldset>
-
-              <fieldset>
-                <textarea placeholder="Mitteilung" name="mitteilung" tabindex="9" rows="5"><?= htmlspecialchars($mitteilung) ?></textarea>
-                <span class="error"><?= isset($errors["mitteilung"]) ? htmlspecialchars($errors["mitteilung"]) : htmlspecialchars($mitteilung_error) ?></span>
-              </fieldset>
-
-
-              <!-- Weitere Personen anmelden -->
-              <fieldset class="checkbox">
-                  <div>
-                      <input class="radio" type="checkbox" id="additionalPerson" name="additionalPerson" tabindex="10">
-                      <label for="additionalPerson">Weitere Person anmelden</label>
-                  </div>
-              </fieldset>
-
-              <!-- Zusätzliche Felder für die weitere Person -->
-              <fieldset id="additionalPersonFields" style="display:none;">                  
-                  <fieldset>
-                      <input placeholder="Vorname&#42;" type="text" name="vorname2" value="<?= htmlspecialchars($vorname2) ?>" tabindex="13">
-                      <span class="error"><?= isset($errors["vorname2"]) ? htmlspecialchars($errors["vorname2"]) : "" ?></span>
-                  </fieldset>
-
-                  <fieldset>
-                      <input placeholder="Name&#42;" type="text" name="name2" value="<?= htmlspecialchars($name2) ?>" tabindex="14">
-                      <span class="error"><?= isset($errors["name2"]) ? htmlspecialchars($errors["name2"]) : "" ?></span>
-                  </fieldset>
-
-                  <fieldset>
-                      <input placeholder="Firma&#42;" type="text" name="firma2" value="<?= htmlspecialchars($firma2) ?>" tabindex="15">
-                      <span class="error"><?= isset($errors["firma2"]) ? htmlspecialchars($errors["firma2"]) : "" ?></span>
-                  </fieldset>
-
-                  <fieldset>
-                      <input placeholder="Email&#42;" type="text" name="email2" value="<?= htmlspecialchars($email2) ?>" tabindex="16">
-                      <span class="error"><?= isset($errors["email2"]) ? htmlspecialchars($errors["email2"]) : "" ?></span>
-                  </fieldset>
-              </fieldset>
-
-              <fieldset>
-                <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Anfrage senden</button>
-              </fieldset>
-              <div class="success"><?= htmlspecialchars($success); ?></div>
-            </form>
-
-            <div id="popup" class="popup">
-              <h1>Vielen Dank für Ihr Interesse!</h1> 
-              <p>Ihre An- oder Abmeldung haben wir erhalten. Gerne werden wir Ihnen kurz vor dem Anlass nochmals die wichtigsten Infos per Mail zustellen.<br><br>
-              Herzliche Grüsse <br> 
-              Jonas Müller <br>
-              Leiter Niederlassung Bern</p>
-              <button id="closePopup">Alles klar!</button>
-            </div>
-      </div> 
     </section>
 
-    <script>
-      function showPopup() {
-          document.getElementById('popup').style.display = 'block';
-      }
-      document.getElementById('closePopup').addEventListener('click', function() {
-          document.getElementById('popup').style.display = 'none';
-      });
-
-      <?php if (isset($success)) { ?>
-          showPopup();
-      <?php } ?>
-    </script>
+    <!-- Form check Repo Form and Footer -->
 
     <?php require_once 'footer.php'; ?>
     <script src="js/vendor/modernizr-3.6.0.min.js"></script>
